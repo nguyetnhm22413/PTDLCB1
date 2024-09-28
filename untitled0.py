643c70863c181351c60e333031e6b0bfab5aabcb
nguyetnhm22413 <nguyetnhm22413@st.uel.edu.vn>
2024-09-28 08:54:01 +0700

    Add files via upload

--- untitled0.py
+++ untitled0.py
@@ -11,5 +11,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+chart_data = pd.DataFrame(np.random.randn(20, 5), columns=["a", "b", "c", "d", "e"])
 st.line_chart(chart_data)
